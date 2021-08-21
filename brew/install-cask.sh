abe0e929766ec4e5e8fdc1c4bebdca1e82b4261c
chocovarun <chocolate.varun@gmail.com>
2021-08-20 22:54:13 -0400

    updated cask - removal

--- brew/install-cask.sh
+++ brew/install-cask.sh
@@ -1,45 +1,45 @@
-#brew cask
-brew cask install dropbox
-brew cask install google-chrome
-brew cask install firefox
-brew cask install spotify
-brew cask install alfred
-brew cask install cheatsheet
-brew cask install keycastr
-brew cask install itsycal
-brew cask install grammarly
-brew cask install ccleaner
-brew cask install keybase
+#brew 
+brew  install dropbox
+brew  install google-chrome
+brew  install firefox
+brew  install spotify
+brew  install alfred
+brew  install cheatsheet
+brew  install keycastr
+brew  install itsycal
+brew  install grammarly
+brew  install ccleaner
+brew  install keybase
 
 #Dev Tools
-brew cask install java
-brew cask install ngrok
-brew cask install iterm2
-brew cask install osxfuse
-brew cask install dbeaver-community
-brew cask install gitkraken
-brew cask install visual-studio-code
-# brew cask install docker
-brew cask install miniconda
+brew  install java
+brew  install ngrok
+brew  install iterm2
+brew  install osxfuse
+brew  install dbeaver-community
+brew  install gitkraken
+brew  install visual-studio-code
+# brew  install docker
+# brew  install miniconda
 
 #Communication
-brew cask install skype
-brew cask install zoomus
-brew cask install blue-jeans
-brew cask install slack
-brew cask install microsoft-teams
-brew cask install basecamp
+brew  install skype
+brew  install zoomus
+brew  install blue-jeans
+brew  install slack
+brew  install microsoft-teams
+brew  install basecamp
 
 # Quick Look Plugins (https://github.com/sindresorhus/quick-look-plugins)
-brew cask install qlcolorcode
-brew cask install qlimagesize
-brew cask install qlmarkdown
-brew cask install qlstephen
-brew cask install qlvideo
-brew cask install quicklook-json
-brew cask install quicklook-csv
-brew cask install suspicious-package
-brew cask install webpquicklook
-brew cask install betterzip
+brew  install qlcolorcode
+brew  install qlimagesize
+brew  install qlmarkdown
+brew  install qlstephen
+brew  install qlvideo
+brew  install quicklook-json
+brew  install quicklook-csv
+brew  install suspicious-package
+brew  install webpquicklook
+brew  install betterzip
 
 
